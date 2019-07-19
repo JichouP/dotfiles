0141b77355f4c9cb4b20df6e3dd9a33a27c228fd
JichouP <akkunn100@gmail.com>
2019-07-19 17:54:47 +0900

    zplug

--- init.sh
+++ init.sh
@@ -9,7 +9,15 @@ if !(type "anyenv" > /dev/null 2>&1); then
   git clone https://github.com/anyenv/anyenv ~/.anyenv
   ~/.anyenv/bin/anyenv init
 fi
-if !(type "zgen" > /dev/null 2>&1); then
-  git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+# if !(type "zgen" > /dev/null 2>&1); then
+#   git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+# fi
+if !(type "git" > /dev/null 2>&1); then
+  sudo apt install git
 fi
+if !(type "gawk" > /dev/null 2>&1); then
+  sudo apt install gawk
+fi
+# install zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 chsh -s $(which zsh)
