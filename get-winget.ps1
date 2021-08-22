1a27d8dcc371c9e00dfd79f3cba94f957865988a
JichouP <akkunn100@gmail.com>
2021-08-22 14:28:03 +0900

    update get-winget.ps1

--- get-winget.ps1
+++ get-winget.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v-0.3.11201-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -OutFile winget.appxbundle -UseBasicParsing
+Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget.appxbundle -UseBasicParsing
 Add-AppxPackage -Path winget.appxbundle
 rm winget.appxbundle
