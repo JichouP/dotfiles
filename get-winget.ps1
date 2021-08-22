7021e26bd2e9c8f6b267e005a0da6157e9b40141
JichouP <akkunn100@gmail.com>
2021-08-22 14:30:35 +0900

    fix

--- get-winget.ps1
+++ get-winget.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget.appxbundle -UseBasicParsing
-Add-AppxPackage -Path winget.appxbundle
-rm winget.appxbundle
+Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget.msixbundle -UseBasicParsing
+Add-AppxPackage -Path winget.msixbundle
+rm winget.msixbundle
